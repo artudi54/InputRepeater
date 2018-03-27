c6d45cbf478683540ad6536d1431feacd60b6352
Artur Pietrzyk <artudi54@gmail.com>
2018-03-27 16:30:17 +0200

    Update main.cpp

--- InputRepeater/main.cpp
+++ InputRepeater/main.cpp
@@ -2,11 +2,6 @@
 #include "InputRepeaterApplication.hpp"
 #include "InputRepeater.hpp"
 
-#if QT_HAS_INCLUDE(<chrono>)
-int a = 6;
-#endif
-
-//todo: review noexcept
 int main(int argc, char *argv[]) {
 
 	InputRepeaterApplication application(argc, argv);
